266b511f5caa1cab8227c3768b03e826e8f02fe9
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-06-02 15:24:38 -0400

    fixed the localhost:8081/users pathway link. So now I can navigate to users where it shows all users and all my followers and people I am following.

--- src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/UserController.java
@@ -1,20 +1,21 @@
 package com.tts.TechTalentTwitter.controller;
 
-import com.tts.TechTalentTwitter.model.Tweet;
-import com.tts.TechTalentTwitter.model.User;
-import com.tts.TechTalentTwitter.service.TweetService;
-import com.tts.TechTalentTwitter.service.UserService;
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.HashMap;
-import java.util.List;
+import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.service.TweetService;
+import com.tts.TechTalentTwitter.service.UserService;
 
 @Controller
-public class UserController{
+public class UserController {
 
     @Autowired
     private UserService userService;
@@ -22,9 +23,11 @@ public class UserController{
     @Autowired
     private TweetService tweetService;
 
-    @GetMapping(value = "/user/{username}")
-    public String getUser(@PathVariable(value="username")String username, Model model){
+    @GetMapping(value = "/users/{username}")
+    public String getUser(@PathVariable(value = "username") String username, Model model) {
         User loggedInUser = userService.getLoggedInUser();
+        User user = userService.findByUsername(username);
+        List<Tweet> tweets = tweetService.findAllByUser(user);
         List<User> following = loggedInUser.getFollowing();
         boolean isFollowing = false;
         for (User followedUser : following) {
@@ -32,30 +35,45 @@ public class UserController{
                 isFollowing = true;
             }
         }
-        model.addAttribute("following", isFollowing);
-        User user = userService.findByUsername(username);
-        List<Tweet> tweets = tweetService.findAllByUser(user);
+        boolean isSelfPage = loggedInUser.getUsername().equals(username);
         model.addAttribute("tweetList", tweets);
         model.addAttribute("user", user);
+        model.addAttribute("following", isFollowing);
+        model.addAttribute("isSelfPage", isSelfPage);
         return "user";
-
     }
-    //We're going to add some special functionality to this page to show how many times each user has tweeted.
-    // This method will take in a list of users and the Model, and update the Model to include tweet counts.
-    // To keep our controller method focused, we're choosing to break out this piece of functionality
+
     @GetMapping(value = "/users")
     public String getUsers(Model model) {
         List<User> users = userService.findAll();
+        User loggedInUser = userService.getLoggedInUser();
+        List<User> usersFollowing = loggedInUser.getFollowing();
+        SetFollowingStatus(users, usersFollowing, model);
         model.addAttribute("users", users);
         SetTweetCounts(users, model);
+
         return "users";
     }
+
     private void SetTweetCounts(List<User> users, Model model) {
-        HashMap<String,Integer> tweetCounts = new HashMap<>();
+        HashMap<String, Integer> tweetCounts = new HashMap<>();
         for (User user : users) {
             List<Tweet> tweets = tweetService.findAllByUser(user);
             tweetCounts.put(user.getUsername(), tweets.size());
         }
         model.addAttribute("tweetCounts", tweetCounts);
+    }
+
+    private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+        HashMap<String, Boolean> followingStatus = new HashMap<>();
+        String username = userService.getLoggedInUser().getUsername();
+        for (User user : users) {
+            if (usersFollowing.contains(user)) {
+                followingStatus.put(user.getUsername(), true);
+            } else if (!user.getUsername().equals(username)) {
+                followingStatus.put(user.getUsername(), false);
+            }
+        }
+        model.addAttribute("followingStatus", followingStatus);
     }
 }
--- src/main/java/com/tts/TechTalentTwitter/model/Tweet.java
+++ src/main/java/com/tts/TechTalentTwitter/model/Tweet.java
@@ -43,9 +43,9 @@ public class Tweet {
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
-//    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
-//    @JoinTable(name = "tweet_tag", joinColumns = @JoinColumn(name = "tweet_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
-//    private List<Tag> tags;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "tweet_tag", joinColumns = @JoinColumn(name = "tweet_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
 
     @NotEmpty(message = "Tweet cannot be empty")
     @Length(max = 280, message = "Tweet cannot have more than 280 characters")
